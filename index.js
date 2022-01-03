8eb89387103edf044f2717ed6bfc6da101077baf
“Julian” <j.makhoul1995@gmail.com>
2022-01-03 10:56:25 +0200

    aa

--- index.js
+++ index.js
@@ -4,58 +4,11 @@ const stats=document.querySelector("stats");
 const nameH=document.querySelector("nameH");
 const changeButton = document.querySelector(".changeButton");
 const card = document.querySelector(".card")
+const newCard = document.querySelector(".new_card")
 var random=1;
 var pokiName=" "
 var status_flag=0;
 
-// form.addEventListener("add", (event)=>{
-//     fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-// .then((response) => {
-//     if (!response.ok) throw new Error(response.status);
-//     return response.json();
-// })
-// .then((pokemonData) => {
-//     status_flag=1;
-//     console.log(pokemonData);
-//     title = document.createElement("h2");
-//     title.textContent = pokemonData.name;
-//     nameH.appendChild(title);
-//     statsArr =pokemonData.stats;
-//     console.log(statsArr)
-//     for(let i=0;i<6;i++) {
-//         let icon =document.createElement("img")
-//         icon.src = 'icon'+i+'.png'
-//         icon.width = 40
-//         console.log(icon.src)
-//         let stat =document.createElement("div")
-//         let discr1=document.createElement("h2")
-//         discr1.innerHTML=statsArr[i].stat.name + " : " + statsArr[i].base_stat;
-//         // let discr2=document.createElement("div")
-//         // discr2.textContent=statsArr[i].base_stat;
-//         discr1.classList.add("h2"+ i)
-//         // discr2.classList.add("h2"+ i)
-//         // stats.appendChild(icon)
-//         discr1.appendChild(icon)
-//         stats.appendChild(discr1)
-        
-//         // stats.appendChild(discr2)
-//     }
-//     searchGif (name)    
-// })
-// .catch((error) => {
-//     status_flag=0;
-//     console.log(error);
-//     if (error.message === "404") {
-//         output.textContent = `⚠️ Couldn't find "${name}"`;
-//         changeButton.style.display = "none";
-//     } else {
-//         output.textContent = "⚠️ Something went wrong";
-//     }
-
-// })
-
-  
-// });
 
 changeButton.addEventListener("click" ,  (event)=>{
     random=Math.ceil(Math.random()*50);
@@ -122,6 +75,66 @@ form.addEventListener("submit", (event) => {
         });
 })
 
+var i = 0
+var statsElementsArr = [stats]
+console.log(newCard)
+newCard.addEventListener("click", (event) => {
+    card.style.visibility= "visible";
+    // output.innerHTML = "";
+    // stats.innerHTML=" ";
+    // nameH.innerHTML= "";
+    statsElementsArr.push(statsElementsArr[0].cloneNode(true))
+    const name = pokiName ;
+    console.log(name);
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then((response) => {
+            if (!response.ok) throw new Error(response.status);
+            return response.json();
+        })
+        .then((pokemonData) => {
+            status_flag=1;
+            console.log(pokemonData);
+            const title = document.createElement("h2");
+            title.textContent = pokemonData.name;
+            nameH.appendChild(title);
+            const statsArr =pokemonData.stats;
+            console.log({statsArr})
+            for(let i=0;i<6;i++) {
+                let icon =document.createElement("img")
+                icon.src = 'icon'+i+'.png'
+                icon.width = 40
+                console.log(icon.src)
+                let stat =document.createElement("div")
+                let discr1=document.createElement("h2")
+                discr1.innerHTML=statsArr[i].stat.name + " : " + statsArr[i].base_stat;
+                // let discr2=document.createElement("div")
+                // discr2.textContent=statsArr[i].base_stat;
+                discr1.classList.add("h2"+ i)
+                // discr2.classList.add("h2"+ i)
+                // stats.appendChild(icon)
+                discr1.appendChild(icon)
+                statsElementsArr[i].appendChild(discr1)
+                i++
+                console.log('btata',i)
+                
+                // stats.appendChild(discr2)
+            }
+            searchGif (name)    
+        })
+        .catch((error) => {
+            status_flag=0;
+            console.log(error);
+            if (error.message === "404") {
+                output.textContent = `⚠️ Couldn't find "${name}"`;
+                changeButton.style.visibility = "none";
+            } else {
+                output.textContent = "⚠️ Something went wrong";
+            }
+
+            
+        });
+})
     function searchGif (name) {
     fetch(`https://api.giphy.com/v1/gifs/search?api_key=nD60pUVFXQt0rkotJYYIZxhUVCxdw6jx&q=${name}`)
         .then((response) => {
