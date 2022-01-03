4e4fa289171149eb516f1056842b4d5535d64d46
Doaa ziadat <doaaziadat@gmail.com>
2022-01-03 10:52:26 +0200

    add new card

--- index.js
+++ index.js
@@ -1,65 +1,77 @@
 const form = document.querySelector("form");
-const output = document.querySelector("output");
-const stats=document.querySelector("stats");
-const nameH=document.querySelector("nameH");
-const changeButton = document.querySelector(".changeButton");
-const card = document.querySelector(".card")
+const output = document.querySelector("output")
+const cards = document.querySelector("cards")
+const clear=document.querySelector(".clear")
 var random=1;
 var pokiName=" "
 var status_flag=0;
 
 
 
-changeButton.addEventListener("click" ,  (event)=>{
-    random=Math.ceil(Math.random()*50);
-    if(status_flag)
-    searchGif(pokiName)
+clear.addEventListener("click" ,() => {
+    cards.innerHTML= " ";
 })
 
 form.addEventListener("submit", (event) => {
-    card.style.visibility= "visible";
-    output.innerHTML = "";
-    stats.innerHTML=" ";
-    nameH.innerHTML= "";
     event.preventDefault();
+    // currentCard.style.visibility= "visible";
+    // output.innerHTML = "";
+    // stats.innerHTML=" ";
+    // nameH.innerHTML= "";
     const formData = new FormData(event.target);
     const name = formData.get("pokemon");
     console.log(formData);
     console.log(name);
     pokiName=name;
+    createCard(name)
 
+});
+
+ function createCard(name) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
         .then((response) => {
             if (!response.ok) throw new Error(response.status);
             return response.json();
         })
         .then((pokemonData) => {
-            status_flag=1;
-            console.log(pokemonData);
+            let card = document.createElement("div");
+            card.classList.add("card");
+
+            let stat =document.createElement("div")
             const title = document.createElement("h2");
             title.textContent = pokemonData.name;
-            nameH.appendChild(title);
-            const statsArr =pokemonData.stats;
-            console.log(statsArr)
+            
+            const statsArr = pokemonData.stats;
+            card.appendChild(title);
             for(let i=0;i<6;i++) {
+                let discr1=document.createElement("h2")
+
                 let icon =document.createElement("img")
                 icon.src = 'icon'+i+'.png'
                 icon.width = 40
-                console.log(icon.src)
-                let stat =document.createElement("div")
-                let discr1=document.createElement("h2")
+        
                 discr1.innerHTML=statsArr[i].stat.name + " : " + statsArr[i].base_stat;
                 // let discr2=document.createElement("div")
                 // discr2.textContent=statsArr[i].base_stat;
                 discr1.classList.add("h2"+ i)
+                discr1.appendChild(icon)
                 // discr2.classList.add("h2"+ i)
                 // stats.appendChild(icon)
-                discr1.appendChild(icon)
-                stats.appendChild(discr1)
-                
-                // stats.appendChild(discr2)
-            }
-            searchGif (name)    
+
+                card.appendChild(discr1)
+            }   
+        
+            searchGif (name,card)
+            
+            changeButton= document.createElement("button")
+            changeButton.innerHTML="get another giphy"
+            changeButton.addEventListener("click" ,  (event)=>{
+                random=Math.ceil(Math.random()*50);
+                searchGif(pokemonData.name,card)
+            })
+            card.appendChild(changeButton)
+            cards.appendChild(card)   
+          
         })
         
 
@@ -74,11 +86,12 @@ form.addEventListener("submit", (event) => {
 
             
         });
+    }
 
           
-    });
+    
 
-    function searchGif (name) {
+    function searchGif (name,currentCard) {
     fetch(`https://api.giphy.com/v1/gifs/search?api_key=nD60pUVFXQt0rkotJYYIZxhUVCxdw6jx&q=${name}`)
         .then((response) => {
             if (!response.ok) throw new Error(response.status);
@@ -98,6 +111,9 @@ form.addEventListener("submit", (event) => {
             );
 
             output.appendChild(gif);
+
+            currentCard.appendChild(gif)
+
         })
 
         .catch((error) => {
